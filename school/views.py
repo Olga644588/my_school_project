b022dc9411ad26b0d17291dbc27591fa87efbd4e
Olga644588 <os3634067@gmail.com>
2026-02-02 00:05:00 +0500

    Изменены файлы

--- school/views.py
+++ school/views.py
@@ -1,13 +1,10 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-
 from .models import Student
-
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    ordering = 'group'
-
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
     return render(request, template, context)
