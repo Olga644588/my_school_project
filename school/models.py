eb8818ea8797f974961cea2fab1cc4aff9154fb9
Olga644588 <os3634067@gmail.com>
2026-02-02 17:19:49 +0500

    Изменен файл

--- school/models.py
+++ school/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Teacher(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
